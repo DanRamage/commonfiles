b6dd7cb9a45653c31346765d98ef21af00abbd57
DanRamage <dan@inlet.geol.sc.edu>
2016-05-20 14:26:45 -0400

    Init checkin.

---
+++ python/wq_prediction_plugin.py
@@ -0,0 +1,19 @@
+import logging.config
+
+from yapsy.IPlugin import IPlugin
+from multiprocessing import Process, Queue, current_process
+
+class wq_prediction_engine_plugin(IPlugin, Process):
+  def __init__(self):
+    Process.__init__(self)
+    IPlugin.__init__(self)
+    self.logger = logging.getLogger(type(self).__name__)
+
+  def inititalize_plugin(self, **kwargs):
+    raise NotImplementedError("Function must be implemented by child.")
+
+  def run_wq_models(self, **kwargs):
+    raise NotImplementedError("Function must be implemented by child.")
+
+  def run(self):
+    return
