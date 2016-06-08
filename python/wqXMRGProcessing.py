b4f1b96a9562142fe3f113fab3466f115296ea4d
DanRamage <dan@inlet.geol.sc.edu>
2016-06-08 11:09:20 -0400

    Fix up s order issues.

--- python/wqXMRGProcessing.py
+++ python/wqXMRGProcessing.py
@@ -988,6 +988,7 @@ def main():
 
     logger = None
     logConfFile = configFile.get('logging', 'xmrg_ingest')
+    print logConfFile
     logger_name = configFile.get('logging', 'xmrg_ingest_logger_name')
     if(logConfFile):
       logging.config.fileConfig(logConfFile)
