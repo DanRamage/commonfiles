f2e6f5a4fc94855bd518dda44a09efcb679998ad
DanRamage <ChiefDan@gmail.com>
2016-10-20 15:15:17 -0400

    ADded sftp ability to get xmrg files.

--- python/wqXMRGProcessing.py
+++ python/wqXMRGProcessing.py
@@ -503,10 +503,10 @@ class wqXMRGProcessing(object):
       self.sftp = False
       self.sftp_user = None
       self.sftp_password = None
-      self.sftp = configFile.get('nexrad_database', 'use_sftp')
+      self.sftp = configFile.getboolean('nexrad_database', 'use_sftp')
       if self.logger:
         self.logger.debug("Use sftp: %s" % (self.sftp))
-      self.sftp_base_directory = configFile.getboolean('nexrad_database', 'sftp_base_directory')
+      self.sftp_base_directory = configFile.get('nexrad_database', 'sftp_base_directory')
       if self.sftp:
         pwd_file = configFile.get('nexrad_database', 'sftp_password_file')
         pwd_config_file = ConfigParser.RawConfigParser()
