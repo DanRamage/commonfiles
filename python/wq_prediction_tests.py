ff3ddc6174ce2d13e542fe9c72bbf4c45c745046
DANRAMAGE <ChiefDan@gmail.com>
2022-05-05 10:46:39 -0400

    We no longer use a MEDIUM classification, either it's LOW or HIGH.

--- python/wq_prediction_tests.py
+++ python/wq_prediction_tests.py
@@ -10,7 +10,7 @@ class predictionLevels(object):
   def __init__(self, value):
     self.__value = value
   def __str__(self):
-    if self.value == self.LOW and self.value < self.HIGH:
+    if self.value >= self.LOW and self.value < self.HIGH:
       return "LOW"
 #    elif self.value == self.MEDIUM:
 #      return "MEDIUM"
